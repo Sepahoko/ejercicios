1b2b94e0ce58c26c20a2a1dc96d59612c78f15ad
cristian <cristianedjang@gmail.com>
2024-11-15 22:51:34 +0100

    the second

---
+++ 01_basico/02_datatypes.dart
@@ -0,0 +1,18 @@
+main(){
+  //===== Números
+  int a = 10;
+  double b = 5.5;
+
+  int? c;
+
+  print(a);
+
+  //===== Strings
+
+  String nombre = 'Tony';
+  String? nombre_2;  
+
+  print(nombre);
+
+
+}
